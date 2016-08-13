d487d1b92a8683632a3421f0a017b793ce0b89e8
ym <kiyoshi.kodama@gmail.com>
2016-08-13 13:47:08 +0900

    Okuribitoの最低限の機能を追加.

--- lib/okuribito.rb
+++ lib/okuribito.rb
@@ -1,5 +1,91 @@
 require "okuribito/version"
+require "yaml"
+require "active_support"
+require "active_support/core_ext"
+require "net/http"
+require "uri"
+require "json"
 
 module Okuribito
-  # Your code goes here...
+  class OkuribitoPatch
+    CLASS_METHOD_SYMBOL = "."
+    INSTANCE_METHOD_SYMBOL = "#"
+    PATTERN = /\A(?<symbol>[#{CLASS_METHOD_SYMBOL}#{INSTANCE_METHOD_SYMBOL}])(?<method_name>.+)\z/
+    WEB_HOOK_URL = "https://hooks.slack.com/services/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"
+    CONSOLE   = true
+    END_POINT = false
+    LOGGING   = false
+
+    module PatchModule
+      def disp_console_by_okuribito(method_name, obj_name, caller_info)
+        return unless CONSOLE
+        puts "#############################################################"
+        puts "# #{obj_name} : #{method_name} is called."
+        puts "#############################################################"
+        puts caller_info
+      end
+
+      def notificate_end_point_by_okuribito(method_name, obj_name, caller_info)
+        return unless END_POINT
+        uri  = URI.parse(WEB_HOOK_URL)
+        params = { text: "#{obj_name}, #{method_name} : #{caller_info[0]}" }
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.start do
+          request = Net::HTTP::Post.new(uri.path)
+          request.set_form_data(payload: params.to_json)
+          http.request(request)
+        end
+      end
+
+      def define_okuribito_patch(method_name)
+        define_method(method_name) do |*args|
+          yield(self.to_s, caller) if block_given?
+          super(*args)
+        end
+      end
+    end
+
+    def apply(yaml_path)
+      yaml = YAML.load_file(yaml_path)
+      yaml.each do |class_name, observe_methods|
+        patch_okuribito(class_name, observe_methods)
+      end
+    end
+
+    def patch_okuribito(class_name, observe_methods)
+      class_name.constantize.class_eval do
+        instance_method_patch = Module.new.extend(PatchModule)
+        class_method_patch    = Module.new.extend(PatchModule)
+        instance_method_patched = 0
+        class_method_patched    = 0
+
+        observe_methods.each do |observe_method|
+          next unless md = PATTERN.match(observe_method)
+          symbol, method_name = md[:symbol], md[:method_name].to_sym
+
+          case symbol
+            when INSTANCE_METHOD_SYMBOL
+              instance_method_patch.module_eval do
+                define_okuribito_patch(method_name) do |obj_name, caller_info|
+                  disp_console_by_okuribito(method_name, obj_name, caller_info)
+                  notificate_end_point_by_okuribito(method_name, obj_name, caller_info)
+                end
+              end
+              instance_method_patched += 1
+            when CLASS_METHOD_SYMBOL
+              class_method_patch.module_eval do
+                define_okuribito_patch(method_name) do |obj_name, caller_info|
+                  disp_console_by_okuribito(method_name, obj_name, caller_info)
+                  notificate_end_point_by_okuribito(method_name, obj_name, caller_info)
+                end
+              end
+              class_method_patched += 1
+          end
+        end
+        prepend instance_method_patch if instance_method_patched > 0
+        singleton_class.prepend class_method_patch if class_method_patched > 0
+      end
+    end
+  end
 end
--- okuribito.gemspec
+++ okuribito.gemspec
@@ -9,15 +9,15 @@ Gem::Specification.new do |spec|
   spec.authors       = ["yasuhiro_matsumura"]
   spec.email         = ["yasuhiro.matsumura@pixta.co.jp"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{TBD}
+  spec.description   = %q{TBD}
+  spec.homepage      = "https://github.com/muramurasan/"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
   if spec.respond_to?(:metadata)
-    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
+    # spec.metadata['allowed_push_host'] = "http://mygemserver.com"
   else
     raise "RubyGems 2.0 or newer is required to protect against public gem pushes."
   end
@@ -30,4 +30,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.12"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_dependency "active_support", "~> 3.0"
 end
