6b648fecba76aed6ab1a482c449d209091dec9d4
kaibazawa.dai <kaibadash@gmail.com>
2016-09-21 21:32:25 +0900

    WIP

--- spec/okuribito_spec.rb
+++ spec/okuribito_spec.rb
@@ -1,11 +1,37 @@
 require 'spec_helper'
 
-describe Okuribito do
-  it 'has a version number' do
-    expect(Okuribito::VERSION).not_to be nil
+class TargetClass
+  def self.self_method
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  def self.unused_self_method
+  end
+
+  def method
+  end
+
+  def unused_method
+  end
+end
+
+describe Okuribito do
+  describe "#apply" do
+    before do
+      okuribito = Okuribito::OkuribitoPatch.new(
+        {
+          console: "back_trace",
+          logging: "tmp/okuribito_test.log",
+          first_prepended: "log/okuribito/first_prepended.log"
+        }
+      )
+      okuribito.apply("spec/support/test_config.yml")
+    end
+
+    context "with static used method" do
+      it do
+        logger = double("Logger", info: nil)
+        expect(logger).to have_recieved(:info).with(an_instance_of(String))
+      end
+    end
   end
 end
