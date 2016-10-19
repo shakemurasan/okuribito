d3e0caaae89064876724e089fa541f4267473b81
ym <kiyoshi.kodama@gmail.com>
2016-10-20 00:38:34 +0900

    gemが作るModuleにメソッド呼び出し状況の保存先を移動 (プロダクションコードを汚染しないようにした)
    1回でも通知が行われたメソッドは、ブロック呼び出しを行わないようにする(インスタンスメソッドも) #27

--- lib/okuribito.rb
+++ lib/okuribito.rb
@@ -15,7 +15,7 @@ module Okuribito
     end
 
     module SimplePatchModule
-      def define_okuribito_patch(_klass, _id, method_name, _opt = {})
+      def define_patch(method_name, _patch, _id, _opt = {})
         define_method(method_name) do |*args|
           yield(to_s, caller) if block_given?
           super(*args)
@@ -24,12 +24,12 @@ module Okuribito
     end
 
     module FunctionalPatchModule
-      def define_okuribito_patch(klass, id, method_name, opt = {})
-        klass.instance_variable_set("@#{method_name}_#{id}_called", false)
+      def define_patch(method_name, patch, id, opt = {})
+        patch.instance_variable_set("@#{method_name}_#{id}_called", false)
         define_method(method_name) do |*args|
-          if block_given? && !klass.instance_variable_get("@#{method_name}_#{id}_called")
+          if block_given? && !patch.instance_variable_get("@#{method_name}_#{id}_called")
             yield(to_s, caller)
-            klass.instance_variable_set("@#{method_name}_#{id}_called", true) if opt[:once_detect]
+            patch.instance_variable_set("@#{method_name}_#{id}_called", true) if opt[:once_detect]
           end
           super(*args)
         end
@@ -50,14 +50,26 @@ module Okuribito
 
       klass.class_eval do
         if opt.present?
-          instance_method_patch = Module.new.extend(FunctionalPatchModule)
-          class_method_patch    = Module.new.extend(FunctionalPatchModule)
+          i_patch_name = "#{class_name}InstancePatch"
+          c_patch_name = "#{class_name}ClassPatch"
+          if FunctionalPatchModule.const_defined?(i_patch_name.to_sym)
+            i_method_patch = Module.new.extend(FunctionalPatchModule)
+          else
+            i_method_patch = FunctionalPatchModule
+                             .const_set(i_patch_name, Module.new.extend(FunctionalPatchModule))
+          end
+          if FunctionalPatchModule.const_defined?(c_patch_name.to_sym)
+            c_method_patch = Module.new.extend(FunctionalPatchModule)
+          else
+            c_method_patch = FunctionalPatchModule
+                             .const_set(c_patch_name, Module.new.extend(FunctionalPatchModule))
+          end
         else
-          instance_method_patch = Module.new.extend(SimplePatchModule)
-          class_method_patch    = Module.new.extend(SimplePatchModule)
+          i_method_patch = Module.new.extend(SimplePatchModule)
+          c_method_patch = Module.new.extend(SimplePatchModule)
         end
-        instance_method_patched = 0
-        class_method_patched    = 0
+        i_method_patched = 0
+        c_method_patched = 0
 
         observe_methods.each do |observe_method|
           next unless md = PATTERN.match(observe_method)
@@ -67,24 +79,24 @@ module Okuribito
           case symbol
           when INSTANCE_METHOD_SYMBOL
             next unless klass.instance_methods.include?(method_name)
-            instance_method_patch.module_eval do
-              define_okuribito_patch(klass, "i", method_name, opt) do |obj_name, caller_info|
+            i_method_patch.module_eval do
+              define_patch(method_name, i_method_patch, "i", opt) do |obj_name, caller_info|
                 callback.call(method_name, obj_name, caller_info)
               end
             end
-            instance_method_patched += 1
+            i_method_patched += 1
           when CLASS_METHOD_SYMBOL
             next unless klass.respond_to?(method_name)
-            class_method_patch.module_eval do
-              define_okuribito_patch(klass, "c", method_name, opt) do |obj_name, caller_info|
+            c_method_patch.module_eval do
+              define_patch(method_name, c_method_patch, "c", opt) do |obj_name, caller_info|
                 callback.call(method_name, obj_name, caller_info)
               end
             end
-            class_method_patched += 1
+            c_method_patched += 1
           end
         end
-        prepend instance_method_patch if instance_method_patched > 0
-        singleton_class.send(:prepend, class_method_patch) if class_method_patched > 0
+        prepend i_method_patch if i_method_patched > 0
+        singleton_class.send(:prepend, c_method_patch) if c_method_patched > 0
       end
     end
   end
