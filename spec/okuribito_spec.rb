daf7871e1691dbc598f1f9354871ea4c3dbc72e4
ym <kiyoshi.kodama@gmail.com>
2016-10-30 22:43:11 +0900

    クラス名と、メソッドシンボルを使うケースについてテスト追加

--- spec/okuribito_spec.rb
+++ spec/okuribito_spec.rb
@@ -8,66 +8,97 @@ describe Okuribito do
   let(:output) { StringIO.new }
   let(:option) { {} }
 
-  before do
-    allow_any_instance_of(Kernel).to receive(:caller).and_return(dummy_caller)
-    okuribito = Okuribito::OkuribitoPatch.new(option) do |method_name, obj_name, caller_info|
-      output.puts "#{obj_name} #{method_name} #{caller_info[0]}"
-    end
-    okuribito.apply(setting_path)
-  end
-
-  describe "#define_okuribito_patch" do
-    subject { output.string.chomp }
-
-    context "when target class method called" do
-      before do
-        TestTarget.deprecated_self_method
+  describe "simple version" do
+    before do
+      allow_any_instance_of(Kernel).to receive(:caller).and_return(dummy_caller)
+      okuribito = Okuribito::OkuribitoPatch.new(option) do |method_name, obj_name, caller_info|
+        output.puts "#{obj_name} #{method_name} #{caller_info[0]}"
       end
-
-      it { is_expected.to eq "TestTarget deprecated_self_method #{dummy_caller[0]}" }
+      okuribito.apply(setting_path)
     end
 
-    context "when target class method called twice" do
-      before do
-        TestTarget.deprecated_self_method
-        TestTarget.deprecated_self_method
-      end
+    describe "#define_okuribito_patch" do
+      subject { output.string.chomp }
 
-      context "(no option)" do
-        it { is_expected.to eq "TestTarget deprecated_self_method #{dummy_caller[0]}\nTestTarget deprecated_self_method #{dummy_caller[0]}" }
-      end
+      context "when target class method called" do
+        before do
+          TestTarget.deprecated_self_method
+        end
 
-      context "(option: once detect)" do
-        let(:option) { { once_detect: true } }
         it { is_expected.to eq "TestTarget deprecated_self_method #{dummy_caller[0]}" }
       end
-    end
 
-    context "when target instance method called" do
-      before do
-        TestTarget.new.deprecated_method
+      context "when target class method called twice" do
+        before do
+          TestTarget.deprecated_self_method
+          TestTarget.deprecated_self_method
+        end
+
+        context "(no option)" do
+          it { is_expected.to eq "TestTarget deprecated_self_method #{dummy_caller[0]}\nTestTarget deprecated_self_method #{dummy_caller[0]}" }
+        end
+
+        context "(option: once detect)" do
+          let(:option) { { once_detect: true } }
+          it { is_expected.to eq "TestTarget deprecated_self_method #{dummy_caller[0]}" }
+        end
       end
 
-      it { is_expected.to match "#<TestTarget:0x[0-9a-f]+> deprecated_method #{dummy_caller[0]}" }
-    end
+      context "when target instance method called" do
+        before do
+          TestTarget.new.deprecated_method
+        end
 
-    context "when target instance method called twice" do
-      let(:test_target) { TestTarget.new }
-
-      before do
-        test_target.deprecated_method
-        test_target.deprecated_method
-        TestTarget.new.deprecated_method
-      end
-
-      context "(no option)" do
-        it { is_expected.to match "#<TestTarget:0x[0-9a-f]+> deprecated_method #{dummy_caller[0]}\n" }
-      end
-
-      context "(option: once detect)" do
-        let(:option) { { once_detect: true } }
         it { is_expected.to match "#<TestTarget:0x[0-9a-f]+> deprecated_method #{dummy_caller[0]}" }
-        it { is_expected.not_to match "#<TestTarget:0x[0-9a-f]+> deprecated_method #{dummy_caller[0]}\n" }
+      end
+
+      context "when target instance method called twice" do
+        let(:test_target) { TestTarget.new }
+
+        before do
+          test_target.deprecated_method
+          test_target.deprecated_method
+          TestTarget.new.deprecated_method
+        end
+
+        context "(no option)" do
+          it { is_expected.to match "#<TestTarget:0x[0-9a-f]+> deprecated_method #{dummy_caller[0]}\n" }
+        end
+
+        context "(option: once detect)" do
+          let(:option) { { once_detect: true } }
+          it { is_expected.to match "#<TestTarget:0x[0-9a-f]+> deprecated_method #{dummy_caller[0]}" }
+          it { is_expected.not_to match "#<TestTarget:0x[0-9a-f]+> deprecated_method #{dummy_caller[0]}\n" }
+        end
+      end
+    end
+  end
+
+  describe "method define version" do
+    before do
+      okuribito = Okuribito::OkuribitoPatch.new(option) do |method_name, _obj_name, _caller_info, class_name, method_symbol|
+        output.puts "#{class_name}#{method_symbol}#{method_name}"
+      end
+      okuribito.apply(setting_path)
+    end
+
+    describe "#define_okuribito_patch" do
+      subject { output.string.chomp }
+
+      context "when target class method called" do
+        before do
+          TestTarget.deprecated_self_method
+        end
+
+        it { is_expected.to eq "TestTarget.deprecated_self_method" }
+      end
+
+      context "when target instance method called" do
+        before do
+          TestTarget.new.deprecated_method
+        end
+
+        it { is_expected.to eq "TestTarget#deprecated_method" }
       end
     end
   end
