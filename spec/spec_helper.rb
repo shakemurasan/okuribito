fc4897b1fc73f9693d45f667363345e0a835307b
ym <kiyoshi.kodama@gmail.com>
2016-11-29 20:20:41 +0900

    SimpleCovがカバレッジ測定できるように修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ require "okuribito"
 require "simplecov"
 require "codeclimate-test-reporter"
 
+dir = File.join(ENV['CIRCLE_ARTIFACTS'] || "build", "coverage")
+SimpleCov.coverage_dir(dir)
+
 SimpleCov.start do
   add_filter "/vendor/"
   add_filter "/spec/"
